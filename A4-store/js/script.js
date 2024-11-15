b592ee97c87d74717442f7d886ca13020020a934
COMPU-S026\daw2 <villedan456@gmail.com>
2024-11-15 19:23:47 +0100

    Finalizaso

--- A4-store/js/script.js
+++ A4-store/js/script.js
@@ -2,21 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', (event) =>{
 
-const passwordField = document.getElementById("password");
-const togglePassword = document.querySelector(".password-toggle-icon i");
-
-togglePassword.addEventListener("click", function () {
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    togglePassword.classList.remove("fa-eye");
-    togglePassword.classList.add("fa-eye-slash");
-  } else {
-    passwordField.type = "password";
-    togglePassword.classList.remove("fa-eye-slash");
-    togglePassword.classList.add("fa-eye");
-  }
-});
-
 window.mostrarImagenModal = function() {
     
     const modal = document.getElementById("modalLogin");
